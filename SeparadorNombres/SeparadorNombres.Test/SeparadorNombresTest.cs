8b2866433b2f38d34004967d0e1a24ce3380ec3d
Roberto Franco <otrebor11rm@hotmail.com>
2018-08-07 09:44:56 -0500

    Test SeparadorNombres

---
+++ SeparadorNombres/SeparadorNombres.Test/SeparadorNombresTest.cs
@@ -0,0 +1,35 @@
+﻿
+namespace SeparadorNombres.Test
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class SeparadorNombresTest
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            var nombres = new List<string> { "JOSE LUIS REYNOSO ALVAREZ" };
+
+            var separador = new SeparadorNombres(nombres);
+            var persona = separador.Personas.First().Value;
+
+            Assert.AreEqual("JOSE LUIS", persona.Nombres);
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            var nombres = new List<string> { "JUAN ROMO" };
+
+            var separador = new SeparadorNombres(nombres);
+            var persona = separador.Personas.First().Value;
+
+            Assert.AreEqual("JUAN", persona.Nombres);
+            Assert.AreEqual("ROMO", persona.Paterno);
+        }
+    }
+}
