d14a364d2cfde743ce330c2e9210317151b36f22
Roberto Franco <otrebor11rm@hotmail.com>
2018-08-07 09:32:45 -0500

    Clase Persona

---
+++ SeparadorNombres/SeparadorNombres/Persona.cs
@@ -0,0 +1,11 @@
+﻿namespace SeparadorNombres
+{
+    public class Persona
+    {
+        public string Materno { get; set; }
+
+        public string Nombres { get; set; }
+
+        public string Paterno { get; set; }
+    }
+}
